1303d5df24bc5a7feab453b9d34dd4b1fcdc2292
Thiagu silva <114629109+Thiagu-Silva@users.noreply.github.com>
2022-10-16 00:56:50 -0300

    Version 3.0

--- JS/Main.js
+++ JS/Main.js
@@ -8,3 +8,12 @@ $('.banner').slick({
     autoplayspeed: 2500
 });
 
+$('.banner-3').slick({
+    dots: true,
+    infinite: true,
+    speed: 400,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplayspeed: 2500
+});
